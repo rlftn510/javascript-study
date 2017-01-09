e2fbe5077e0cdf88fe755771882dd13e84eb0378
galgale <rlftn510@gmail.com>
2017-01-09 20:43:05 +0900

    window객체를 이용한 팝업창 생성 //스크롤바랑 웍셔너리 조정이 안됨

---
+++ pop-up/js/pop-upScript.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Gil Su on 2017-01-09.
+ */
+var doc = document;
+var popLink = doc.getElementById('popLink');
+popLink.onclick = function () {
+    var popOption = 'width = 500, height = 300, left = 300, top = 400, scrollbars=no, resizable=no';
+    window.open('popUp.html', '광고창', popOption)
+}
