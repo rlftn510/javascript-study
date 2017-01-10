1f5925c62d4d3e3e9dae8f779f165b925fdb101b
galgale <rlftn510@gmail.com>
2017-01-10 17:23:57 +0900

    롤링 previous, next 버튼을 통한 변화

--- rolling/js/rollingScript.js
+++ rolling/js/rollingScript.js
@@ -4,18 +4,18 @@
 var doc = document;
 var ulList = doc.getElementById('ulList');
 var list = ulList.getElementsByTagName('li');
-console.log(ulList.firstChild.nextSibling)
-console.log(ulList.lastChild.previousSibling)
-console.log('asd')
+console.log(ulList.firstChild.nextSibling);
+console.log(ulList.lastChild.previousSibling);
 var prevBtn = doc.getElementById('prevBtn');
 var nextBtn = doc.getElementById('nextBtn');
 
+ulList.style.width = list[0].offsetWidth * list.length + (list.length*10-10) +"px";
 
 prevBtn.onclick = function () {
-    ulList.appendChild(list[0])
-}
+    ulList.appendChild(list[0]);
+};
 nextBtn.onclick = function () {
-    ulList.push(list[list.length - 1])
-}
+    ulList.insertBefore(list[list.length - 1], list[0]);
+};
 
 
