c60d03c89b038a6f8d7f70e21f9001eab3565951
galgale <rlftn510@gmail.com>
2017-01-10 18:42:39 +0900

    랜덤박스 점수 가져오기

---
+++ randumSquare/js/randumSquareScript.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Gil Su on 2017-01-10.
+ */
+var doc = document;
+var ulList = doc.getElementById('ulList');
+var list = ulList.getElementsByTagName('li');
+var score = doc.getElementById('score');
+
+ulList.style.width = list[0].offsetWidth * list.length + (list.length*10-10) +"px";
+
+for(var i = 0; i < list.length; i++){
+    var rd = Math.floor(Math.random()*1000);
+    var randumText = doc.createTextNode(rd);
+    list[i].appendChild(randumText)
+    list[i].onclick = function () {
+        var liScore = doc.createTextNode(this.innerHTML);
+        if(score.innerHTML == ''){
+            score.appendChild(liScore)
+        } else {
+            score.innerHTML = "";
+            score.appendChild(liScore)
+        }
+    }
+}
+
+var galgalE = {
+    randumNumber : function () {
+        var rd = Math.floor(Math.random()*1000);
+        var randumText = doc.createTextNode(rd)
+        return randumText
+    }
+}
