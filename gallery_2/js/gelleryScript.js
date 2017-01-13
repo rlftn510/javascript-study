0a2ef6988c968b5c3359d236d6eb8a3b9f5f4097
galgale <rlftn510@gmail.com>
2017-01-13 18:51:19 +0900

    갤러리 마크업 및 스크립트 코드(엘리먼트 생성및 삭제) 수정

--- gallery_2/js/gelleryScript.js
+++ gallery_2/js/gelleryScript.js
@@ -4,29 +4,47 @@
 var doc = document;
 var main = doc.getElementById('main');
 var listImg =main.getElementsByTagName('img');
-var viewer = doc.getElementById('viewer');
-var holdImg = doc.getElementById('holdImg');
 
 
 var listLen = listImg.length;
 for (var i = 0; i < listLen; i++){
     listImg[i].onclick = function () {
-        viewer.style.display = 'block';
-        console.log(this.nodeName)
+        galgalE.createShow();
+        var show = doc.getElementById('show');
+        var holdImg = doc.getElementById('holdImg');
+        galgalE.cloneImg();
         var imgSrc = this.getAttribute('src');
-        holdImg.setAttribute('src' , imgSrc)
+        holdImg.setAttribute('src' , imgSrc);
+        var viewer = doc.getElementById('viewer');
+        galgalE.removeShow()
     }
 }
 
 
 var galgalE = {
-    closeView : function () {
-    viewer.addEventListener('click', function () {
-        viewer.style.display = 'none'
-    }, true)
-    holdImg.addEventListener('click', function () {
-        viewer.style.display = 'block'
-    }, true)
+        removeShow : function () {
+        viewer.onclick = function () {
+            var bodyy = doc.getElementsByTagName('body')[0];
+            bodyy.removeChild(show)
+        }
+    },
+
+    createShow : function () {
+    var crediv = doc.createElement('div');
+    crediv.setAttribute('id', 'show');
+    var crediv_1 = doc.createElement('div');
+    crediv_1.setAttribute('id', 'viewer');
+    var creImg = doc.createElement('img');
+    creImg.setAttribute('id', 'holdImg');
+    crediv.appendChild(crediv_1);
+    crediv.appendChild(creImg);
+    var bodyy = doc.getElementsByTagName('body')[0];
+    bodyy.appendChild(crediv);
+    },
+
+    cloneImg : function () {
+        show.style.display = 'block';
+
     }
 }
-galgalE.closeView()
+
