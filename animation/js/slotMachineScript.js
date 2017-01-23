75126ca0698270ca96068098a3b445671e5d2ec4
galgale <rlftn510@gmail.com>
2017-01-23 18:14:20 +0900

    css애니메이션/javaScript을 이용한 슬롯머신 제작

--- animation/js/slotMachineScript.js
+++ animation/js/slotMachineScript.js
@@ -9,15 +9,26 @@ btn.onclick = function () {
     var score = doc.getElementById('score');
     var scLi= score.getElementsByTagName('li');
     for (var i = 0; i < scLi.length; i++){
-        scLi[i].innerHTML = randumNum()
+        scLi[i].innerHTML = galglaE.randumNum()
     }
     score.style.display = 'block'
-}
+};
 
-function randumNum() {
-    var slot_1 = doc.getElementById('slot_1');
-    var slotLi = slot_1.getElementsByTagName('li');
-    var randumNum = Math.floor(Math.random()*slotLi.length);
-    var test =  slotLi[randumNum].innerHTML;
-    return test
-}
+
+var galglaE = {
+    restart : function () {
+    var restart = doc.getElementById('restart');
+    var score = doc.getElementById('score');
+        restart.onclick= function () {
+            score.style.display = 'none'
+        }
+    },
+    randumNum : function () {
+        var slot_1 = doc.getElementById('slot_1');
+        var slotLi = slot_1.getElementsByTagName('li');
+        var randumNum = Math.floor(Math.random()*slotLi.length);
+        var test =  slotLi[randumNum].innerHTML;
+        return test
+    }
+};
+galglaE.restart();
