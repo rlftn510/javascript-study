78db876cc09c1042c83e1d156abe71790262c195
galgale <rlftn510@gmail.com>
2017-02-03 20:04:34 +0900

    codepen URL 추가 및 코드분할

--- 사천성/README.md
+++ 사천성/README.md
@@ -1,7 +1,10 @@
-# 같은 그림 맞추기(퍼즐)
-1. 그림을 클릭시 클릭 변화를 주는 compare class 추가
-2. 클래스가 compare가 2개일 경우 해당 클래스의 backgroundColor 비교
-3. backgroundColor가 다를시 경고창 노출 후 compare class 초기화
-4. backgroundColor가 같을 시 backgroundColor값 white를 주어 사라지는 효과
+# 같은 카드 맞추기(퍼즐)
+1. 카드를 클릭시 클릭 변화를 주는 compare class 추가(카드 불투명)
+2. 카드 클래스 compare가 2개일 경우 해당 클래스의 backgroundColor 비교
+3. 카드색깔이 다를시 경고표기(backgroundColor가 다를시 경고창 노출 후 compare class 초기화)
+4. backgroundColor가 같을 시 backgroundColor값 white를 주어 카드 사라지는 효과
 5. white 값이 되면서 compare class 초기화
 6. 반복 진행
+
+codepen URL:(http://codepen.io/GalGalE/pen/bgMrob)
+
--- 사천성/js/colorJs_2.js
+++ 사천성/js/colorJs_2.js
@@ -6,27 +6,29 @@ var tdList = doc.getElementsByTagName('td');
 for (var i = 0; i < tdList.length; i++){
     tdList[i].onclick = function () {
         this.setAttribute('class', 'compare');
-        colorCompare()
+        galgalE.colorCompare()
     }
 }
 
-function colorCompare() {
-    var classLen = [];
-    for (var j = 0; j < tdList.length; j++){
-        if (tdList[j].className == 'compare'){
-            classLen.push(tdList[j]);
-            if (classLen.length === 2){
-                var firstColor = classLen[0].style.backgroundColor;
-                var secondColor = classLen[1].style.backgroundColor;
-                if (firstColor == secondColor){
-                    for (var k = 0; k < classLen.length; k++){
-                        classLen[k].style.backgroundColor = 'white';
-                        classLen[k].setAttribute('class', '')
-                    }
-                } else {
-                    alert('땡!!!!!!!!!!!!');
-                    for (var p = 0; p < classLen.length; p++){
-                        classLen[p].setAttribute('class', '')
+var galgalE = {
+    colorCompare : function () {
+        var classLen = [];
+        for (var j = 0; j < tdList.length; j++){
+            if (tdList[j].className == 'compare'){
+                classLen.push(tdList[j]);
+                if (classLen.length === 2){
+                    var firstColor = classLen[0].style.backgroundColor;
+                    var secondColor = classLen[1].style.backgroundColor;
+                    if (firstColor == secondColor){
+                        for (var k = 0; k < classLen.length; k++){
+                            classLen[k].style.backgroundColor = 'white';
+                            classLen[k].setAttribute('class', '')
+                        }
+                    } else {
+                        alert('땡!!!!!!!!!!!!');
+                        for (var p = 0; p < classLen.length; p++){
+                            classLen[p].setAttribute('class', '')
+                        }
                     }
                 }
             }
